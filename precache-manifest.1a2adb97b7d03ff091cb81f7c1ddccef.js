42ca9e4609582b03ae043395f90f0375b432dff0
Aleksander Grabovaj <aleksander.grabovaj@gmail.com>
2022-09-18 20:34:02 +0200

    Updates

---
+++ precache-manifest.1a2adb97b7d03ff091cb81f7c1ddccef.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7baabe154b9e8373b628f340c3c90ce",
+    "url": "/waiter-s-pad/index.html"
+  },
+  {
+    "revision": "6b3b7cf3181b46aba678",
+    "url": "/waiter-s-pad/static/css/2.0f7872f5.chunk.css"
+  },
+  {
+    "revision": "1d0bc056faccb9576f3a",
+    "url": "/waiter-s-pad/static/css/main.a3854d60.chunk.css"
+  },
+  {
+    "revision": "6b3b7cf3181b46aba678",
+    "url": "/waiter-s-pad/static/js/2.5fe7cc17.chunk.js"
+  },
+  {
+    "revision": "ca7aee1e270dc59e66568294284cf80d",
+    "url": "/waiter-s-pad/static/js/2.5fe7cc17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d0bc056faccb9576f3a",
+    "url": "/waiter-s-pad/static/js/main.2849e86e.chunk.js"
+  },
+  {
+    "revision": "867fa3d542e822da4793",
+    "url": "/waiter-s-pad/static/js/runtime-main.9a8cbc3d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/waiter-s-pad/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/waiter-s-pad/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/waiter-s-pad/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/waiter-s-pad/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/waiter-s-pad/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
